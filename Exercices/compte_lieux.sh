24c03b3a8b1c3c4750cd9779ea6647bbe98c3b52
IvanMiao <reneisaipa@gmail.com>
2024-10-20 23:07:37 +0200

    ex03

--- Exercices/compte_lieux.sh
+++ Exercices/compte_lieux.sh
@@ -4,4 +4,14 @@ YEAR=$1
 MONTH=$2
 NUM=$3
 
+if ! [[ $YEAR =~ ^2016|7|8$ ]]; then
+	echo "ERROR! Enter a new year."
+	exit 1
+fi
+
+if ! [[ $MONTH =~ ^((0[1-9])|(1[0-2]))$ ]]; then
+	echo "ERROR! Enter a new month."
+	exit 1
+fi
+
 cat ./Exercice1/ann/$YEAR/$MONTH/*.ann | grep Location | cut -f3 | sort | uniq -c | sort -nr | head -n $NUM
--- Exercices/compte_par_type.sh
+++ Exercices/compte_par_type.sh
@@ -3,4 +3,9 @@
 ENTITY=$1
 YEAR=$2
 
+if ! [[ "$YEAR" =~ ^(2016|2017|2018)$ ]]; then
+	echo "ERROR! Enter a new year."
+	exit 1
+fi
+
 cat ./Exercice1/ann/$YEAR/*/*.ann | grep $ENTITY | wc | awk '{print $1}'
