eaa14af8622f69f6c2cbc53e0610ccfef24c3f33
IvanMiao <reneisaipa@gmail.com>
2024-11-11 23:48:51 +0100

    mini

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -13,10 +13,32 @@ if [ ! -f "$1" ] || [ ! -r "$1" ]; then
     exit 1
 fi
 
+
+get_info() {
+    local url="$1"
+
+    # Récupérer le code HTTP
+    local code_http=$(curl -o /dev/null -s -w "%{http_code}" "$url")
+
+    # Récupérer l'encodage (si présent)
+    local encodage=$(curl -sI "$url" | grep -i "Content-Type" | grep -o "charset=[^;]*" | cut -d= -f2)
+    if [[ -z "$encodage" ]]; then
+        encodage="Inconnu"
+    fi
+
+    # Récupérer le contenu et compter les mots
+    local contenu=$(curl -s "$url")
+    local nombre_mots=$(echo "$contenu" | wc -w)
+
+    # Retourner les informations
+    echo -e "${COUNT}\t${url}\t${code_http}\t${encodage}\t${nombre_mots}"
+}
+
+
 COUNT=1
 
 while read -r line;
 do
-  echo -e "${COUNT}\t${line}";
-  ((COUNT++))
+	get_info "${line}"
+	((COUNT++))
 done < "$1"
